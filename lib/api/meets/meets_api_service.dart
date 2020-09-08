cd90bf59178003cd9f75cd1a0e9bf016a19d8d28
Abhishek R <abhishek@worknetwork.in>
2020-09-08 13:33:33 +0530

    cleanup

--- lib/api/meets/meets_api_service.dart
+++ lib/api/meets/meets_api_service.dart
@@ -1,58 +1,9 @@
 import 'package:chopper/chopper.dart';
-import 'package:json_annotation/json_annotation.dart';
 
 import 'package:worknetwork/api/interceptors/authorized_interceptor.dart';
 import 'package:worknetwork/constants/app_constants.dart';
-import 'package:worknetwork/models/meeting/meeting_model.dart';
 
 part 'meets_api_service.chopper.dart';
-part 'meets_api_service.g.dart';
-
-@JsonSerializable()
-class MeetingsResponse {
-  final int pk;
-
-  final String title;
-
-  @JsonKey(name: 'week_start_date')
-  final String weekStartDate;
-
-  @JsonKey(name: 'week_end_date')
-  final String weekEndDate;
-
-  @JsonKey(name: 'is_registration_open')
-  final bool isRegistrationOpen;
-
-  @JsonKey(name: 'is_active')
-  final bool isActive;
-
-  @JsonKey(name: 'available_time_slots')
-  final Map<String, List<TimeSlot>> availableTimeSlots;
-
-  final List<MeetingObjective> objectives;
-
-  final List<Interest> interests;
-
-  @JsonKey(name: 'user_preferences', nullable: true)
-  final UserMeetingPreference userPreferences;
-
-  MeetingsResponse({
-    this.pk,
-    this.title,
-    this.weekStartDate,
-    this.weekEndDate,
-    this.isRegistrationOpen,
-    this.isActive,
-    this.availableTimeSlots,
-    this.objectives,
-    this.interests,
-    this.userPreferences,
-  });
-
-  factory MeetingsResponse.fromJson(Map<String, dynamic> json) =>
-      _$MeetingsResponseFromJson(json);
-  Map<String, dynamic> toJson() => _$MeetingsResponseToJson(this);
-}
 
 @ChopperApi(baseUrl: '/resources/')
 abstract class MeetsApiService extends ChopperService {
