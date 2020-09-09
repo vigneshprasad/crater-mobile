e3f9bfe81f4894e3218abddbdb0e5d490c352475
Abhishek R <abhishek@worknetwork.in>
2020-09-09 12:39:03 +0530

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:kiwi/kiwi.dart';
-import 'package:worknetwork/core/local_storage/local_storage.dart';
 
+import 'core/local_storage/local_storage.dart';
 import 'core/widgets/root_app.dart';
 import 'di/injector.dart';
 import 'utils/simple_bloc_observer.dart';
