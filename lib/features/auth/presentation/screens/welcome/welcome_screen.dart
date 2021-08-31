be36ab40946e7e18ab26f79476a9529ae172fc9c
Ram Suthar <reallyram@gmail.com>
2021-08-31 12:54:53 +0530

    enable signup in debug

--- lib/features/auth/presentation/screens/welcome/welcome_screen.dart
+++ lib/features/auth/presentation/screens/welcome/welcome_screen.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -352,7 +353,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                           ),
                         ),
                       ),
-                    if (!isSignUp)
+                    if (!isSignUp || !kReleaseMode)
                       BaseContainer(
                         child: SizedBox(
                           width: buttonWidth,
@@ -363,7 +364,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                             isSignUp: isSignUp,
                             onPressed: () {
                               Navigator.of(context).pop();
-                              _openSignupAuthScreen(false, context);
+                              _openSignupAuthScreen(isSignUp, context);
                             },
                           ),
                         ),
