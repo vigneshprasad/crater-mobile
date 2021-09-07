ecb3b07c51033188730f76abb47798d8f080a003
Ram Suthar <ram@worknetwork.in>
2021-09-07 16:47:31 +0530

    add dyte meeting in clubs screen

--- lib/features/club/presentation/screens/clubs/clubs_screen.dart
+++ lib/features/club/presentation/screens/clubs/clubs_screen.dart
@@ -1,13 +1,25 @@
+import 'package:dyte_client/dyte.dart';
+import 'package:dyte_client/dyteMeeting.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:dyte
+
 
 class ClubsScreen extends HookWidget {
   
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: 
+      child: SizedBox(
+            child: DyteMeeting(
+              roomName: '',
+              authToken: '',
+              onInit: (DyteMeetingHandler meeting) async {
+                meeting.events.on('meetingEnd', context, (ev, c) {
+                  // AutoRouter.of(context).pop();
+                });
+              },
+            ),
+          ) 
     );
   }
 }
